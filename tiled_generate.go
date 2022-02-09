218efa73661000734ff65d46b8e5c612bb478c47
aninggo <aninggo@gmail.com>
2022-02-09 18:24:00 +0800

    update

--- tiled_generate.go
+++ tiled_generate.go
@@ -1,1 +1,9 @@
 package contour
+
+type TiledContourGenerateOptions struct {
+	ContourGenerateOptions
+}
+
+func TiledContourGenerate(r RasterProvider, wf GeometryWriter, options TiledContourGenerateOptions) error {
+	return nil
+}
